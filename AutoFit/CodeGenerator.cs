bab857d1fee05c9d6678cb09f38fc51f0344e4b4
Thijs Brobbel <thijsbrobbel@gmail.com>
2019-10-12 23:31:11 +0200

    Extract OpenApiParser.

--- AutoFit/CodeGenerator.cs
+++ AutoFit/CodeGenerator.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text.Json;
 
 namespace AutoFit
@@ -29,66 +27,8 @@ namespace AutoFit
 
         private void GenerateDto(JsonProperty jsonDtoDefinition)
         {
-            var propertyDefinitions = jsonDtoDefinition.Value.GetProperty("properties");
-            var dtoProperties = propertyDefinitions.EnumerateObject().Select(ToPropertyDefinition);
-
-            var dtoDefinition = new DtoDefinition
-            {
-                Name = jsonDtoDefinition.Name,
-                Properties = dtoProperties
-            };
-
+            var dtoDefinition = OpenApiParser.ParseDefinition(jsonDtoDefinition);
             _dtoGenerator.EmitDto(dtoDefinition);
-        }
-
-        private static PropertyDefinition ToPropertyDefinition(JsonProperty propertyDefinition)
-        {
-            return new PropertyDefinition
-            {
-                TypeName = ParseDtoType(propertyDefinition.Value),
-                IdentifierName = Capitalize(propertyDefinition.Name)
-            };
-        }
-
-        private static string ParseDtoType(JsonElement propertyDefinitionElement)
-        {
-            var type = TryGetPropertyOrDefault(propertyDefinitionElement, "type");
-            var format = TryGetPropertyOrDefault(propertyDefinitionElement, "format");
-
-            return type switch
-            {
-                "number" => propertyDefinitionElement.GetProperty("format").ToString(),
-                "string" when format != null => ParseFormat(format),
-                null => ParseReferenceToOtherDefinition(propertyDefinitionElement),
-                _ => type,
-            };
-        }
-
-        private static string ParseReferenceToOtherDefinition(JsonElement propertyDefinitionElement)
-        {
-            var reference = propertyDefinitionElement.GetProperty("$ref").ToString();
-            return reference.Substring("#/definitions/".Length);
-        }
-
-        private static string TryGetPropertyOrDefault(JsonElement propertyDefinitionElement, string jsonPropertyName)
-        {
-            return propertyDefinitionElement.TryGetProperty(jsonPropertyName, out var format)
-                ? format.ToString()
-                : null;
-        }
-
-        private static string ParseFormat(string format)
-        {
-            return format switch
-            {
-                "date-time" => "DateTime",
-                _ => throw new NotSupportedException()
-            };
-        }
-
-        private static string Capitalize(string name)
-        {
-            return name.Length < 2 ? name : char.ToUpper(name[0]) + name.Substring(1);
         }
     }
 }
---
+++ AutoFit/OpenApiParser.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+
+namespace AutoFit
+{
+    public static class OpenApiParser
+    {
+        public static DtoDefinition ParseDefinition(JsonProperty jsonDefinition)
+        {
+            var propertyDefinitions = jsonDefinition.Value.GetProperty("properties");
+            var dtoProperties = propertyDefinitions.EnumerateObject().Select(ToPropertyDefinition);
+
+            var dtoDefinition = new DtoDefinition
+            {
+                Name = jsonDefinition.Name,
+                Properties = dtoProperties
+            };
+
+            return dtoDefinition;
+        }
+
+        private static PropertyDefinition ToPropertyDefinition(JsonProperty propertyDefinition)
+        {
+            return new PropertyDefinition
+            {
+                TypeName = ParseDtoType(propertyDefinition.Value),
+                IdentifierName = Capitalize(propertyDefinition.Name)
+            };
+        }
+
+        private static string ParseDtoType(JsonElement propertyDefinitionElement)
+        {
+            var type = TryGetPropertyOrDefault(propertyDefinitionElement, "type");
+            var format = TryGetPropertyOrDefault(propertyDefinitionElement, "format");
+
+            return type switch
+            {
+                "number" => propertyDefinitionElement.GetProperty("format").ToString(),
+                "string" when format != null => ParseFormat(format),
+                null => ParseReferenceToOtherDefinition(propertyDefinitionElement),
+                _ => type,
+            };
+        }
+
+        private static string ParseReferenceToOtherDefinition(JsonElement propertyDefinitionElement)
+        {
+            var reference = propertyDefinitionElement.GetProperty("$ref").ToString();
+            return reference.Substring("#/definitions/".Length);
+        }
+
+        private static string TryGetPropertyOrDefault(JsonElement propertyDefinitionElement, string jsonPropertyName)
+        {
+            return propertyDefinitionElement.TryGetProperty(jsonPropertyName, out var format)
+                ? format.ToString()
+                : null;
+        }
+
+        private static string ParseFormat(string format)
+        {
+            return format switch
+            {
+                "date-time" => "DateTime",
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        private static string Capitalize(string name)
+        {
+            return name.Length < 2 ? name : char.ToUpper(name[0]) + name.Substring(1);
+        }
+    }
+}
